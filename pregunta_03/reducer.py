54fdcdcf351d119cb94d8cb965bf1d32a5f6ee5d
ALEJANDRO MONTOYA <amontoyar@unal.edu.co>
2023-05-30 18:44:39 -0500

    Mapper y reducer pregunta_03

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,23 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#
+# >>> Escriba el codigo del reducer a partir de este punto <<<
+#
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.strip().split("\t")
+        sys.stdout.write("{},{}\n".format(val, key))
