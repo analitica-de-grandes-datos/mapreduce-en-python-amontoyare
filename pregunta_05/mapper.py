a0cb880390c91c36a4afed1c1567e47b5cd22199
ALEJANDRO MONTOYA <amontoyar@unal.edu.co>
2023-05-30 20:19:15 -0500

    Mapper y reducer pregunta_05

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+
+        columns = line.split('   ')
+        
+        sys.stdout.write("{}\t1\n".format(columns[1][5:7]))
