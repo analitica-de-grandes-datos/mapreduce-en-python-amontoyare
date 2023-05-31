a3a486770def5769598a534813978639d4dbe9e3
ALEJANDRO MONTOYA <amontoyar@unal.edu.co>
2023-05-30 22:49:03 -0500

    Mapper y reducer pregunta_08

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,47 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    sum = 0
+    count = 0
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            sum += val
+            count += 1
+
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, sum, sum/count))
+
+            curkey = key
+            sum = val
+            count = 1
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, sum, sum/count))
