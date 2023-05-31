5add71714db1d258dd6d85fe900e82cd4dce613f
ALEJANDRO MONTOYA <amontoyar@unal.edu.co>
2023-05-30 22:05:52 -0500

    Mapper y reducer pregunta_07

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    data = []
+    
+    for line in sys.stdin:
+        line = line.strip()
+        columns = line.split("\t")
+        data.append((columns[0], columns[2], int(columns[1])))
+
+    sorted_data = sorted(data, key=lambda x: (x[0], x[2]))
+
+    for item in sorted_data:
+        sys.stdout.write("{}\t{}\t{}\n".format(item[0], item[1], item[2]))
