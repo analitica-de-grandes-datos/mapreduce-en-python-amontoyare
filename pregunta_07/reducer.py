c5d1fd171044c12c9efe1f05b0e78d334d82aeeb
ALEJANDRO MONTOYA <amontoyar@unal.edu.co>
2023-05-30 22:36:02 -0500

    Output pregunta_07

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == '__main__':
 
     data = []
-    
+
     for line in sys.stdin:
         line = line.strip()
         columns = line.split("\t")
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     sorted_data = sorted(data, key=lambda x: (x[0], x[2]))
 
     for item in sorted_data:
-        sys.stdout.write("{}\t{}\t{}\n".format(item[0], item[1], item[2]))
+        sys.stdout.write("{}   {}   {}\n".format(item[0], item[1], item[2]))
